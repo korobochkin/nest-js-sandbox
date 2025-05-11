b76fd843e80aeb1ae88d29d1c0702777fa46b502
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2025-05-11 19:53:28 +0400

    slow-requests method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('slow-requests')
+  async slowRequests(): Promise<void> {
+    await this.appService.slowRequests();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AppDataSource } from './typeorm/data-source';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async slowRequests(): Promise<void> {
+    await AppDataSource.query("SELECT pg_sleep(10)");
   }
 }
